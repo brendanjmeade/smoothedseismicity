9a7ce03a4d6cf9bd66999b6ded9ece0a29561c61
brendanjmeade <brendanjmeade@users.noreply.github.com>
2021-03-03 17:28:15 -0500

    Small catalog selection for speed and some basic H2006 parameters

--- smoothed.py
+++ smoothed.py
@@ -1,11 +1,16 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.spatial import distance_matrix
 
 filename = "EHDD_old.csv"
 df = pd.read_csv(filename)
 df["datetime"] = pd.to_datetime(df[["year", "month", "day", "hour", "minute", "second"]])
 timespan_years = (df.datetime.max() - df.datetime.min()) / np.timedelta64(1, "Y")
+
+# JUST FOR THE TIME BEING
+# CLIP TO EVENTS LARGER THAN 5 FOR SPEED
+df[df.magnitude>5.0]
 
 # Bins from Helmstetter et al. 2006
 grid_spacing = 0.05
@@ -19,8 +24,15 @@ H[np.isinf(H)] = np.nan
 
 plt.close("all")
 plt.figure()
-plt.imshow(H, interpolation='nearest', origin='lower', extent=[lon_edges[0], lon_edges[-1], lat_edges[0], lat_edges[-1]])
+plt.imshow(H, interpolation="nearest", origin="lower", extent=[lon_edges[0], lon_edges[-1], lat_edges[0], lat_edges[-1]])
 plt.show(block=False)
 plt.xlabel("longitude")
 plt.ylabel("latitude")
 plt.colorbar(label="earthquakes per year");
+
+
+# Helmstetter et al. 2006 calcuations
+# Do we have to loop over all earthquake pairs?
+# Do we do distance calculations with grid cell centroids or actual earthquake locations?
+N = len(df) # number of earthquakes (H2006, eq 1)
+d0 = grid_spacing # differential degrees for (H2006, eq 3)

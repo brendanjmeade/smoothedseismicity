225a4fba3a220ff6d47533a3f9b3b01b50a4aee6
brendanjmeade <brendanjmeade@users.noreply.github.com>
2021-03-04 21:26:31 -0500

    Basic mu_star calcuation

--- smoothed.py
+++ smoothed.py
@@ -1,8 +1,22 @@
-import sys
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.spatial import distance_matrix
+
+plt.close("all")
+
+
+def quick_plot(field, colorbar_string, lon_edges, lat_edges):
+    plt.figure()
+    plt.imshow(
+        field,
+        interpolation="nearest",
+        origin="lower",
+        extent=[lon_edges[0], lon_edges[-1], lat_edges[0], lat_edges[-1]],
+    )
+    plt.show(block=False)
+    plt.xlabel("longitude")
+    plt.ylabel("latitude")
+    plt.colorbar(label=colorbar_string)
 
 
 def calc_kernel_influence(src_lon, src_lat, target_lon, target_lat, d):
@@ -16,69 +30,71 @@ def calc_kernel_influence(src_lon, src_lat, target_lon, target_lat, d):
     return Kr
 
 
-filename = "EHDD_old.csv"
-df = pd.read_csv(filename)
-df["datetime"] = pd.to_datetime(
-    df[["year", "month", "day", "hour", "minute", "second"]]
-)
-timespan_years = (df.datetime.max() - df.datetime.min()) / np.timedelta64(1, "Y")
-
-# JUST FOR THE TIME BEING
-# CLIP TO EVENTS LARGER THAN 5 FOR SPEED
-df = df[df.magnitude > 5.0]
-
-# Bins from Helmstetter et al. 2006
-grid_spacing = 0.05
-lon_edges = np.arange(360.0 - 121.55, 360.0 - 114.45 + grid_spacing, grid_spacing)
-lat_edges = np.arange(32.45, 36.65 + grid_spacing, grid_spacing)
-H, _, _ = np.histogram2d(
-    df.longitude.values, df.latitude.values, bins=(lon_edges, lat_edges)
-)
-H = H.T
-H = H / timespan_years
-H = np.log10(H)
-H[np.isinf(H)] = np.nan
-
-plt.close("all")
-plt.figure()
-plt.imshow(
-    H,
-    interpolation="nearest",
-    origin="lower",
-    extent=[lon_edges[0], lon_edges[-1], lat_edges[0], lat_edges[-1]],
-)
-plt.show(block=False)
-plt.xlabel("longitude")
-plt.ylabel("latitude")
-plt.colorbar(label="earthquakes per year")
+def read_data():
+    filename = "EHDD_old.csv"
+    df = pd.read_csv(filename)
+    df["datetime"] = pd.to_datetime(
+        df[["year", "month", "day", "hour", "minute", "second"]]
+    )
+    return df
 
 
-# Helmstetter et al. 2006 calcuations
-# Do we have to loop over all earthquake pairs?
-# Do we do distance calculations with grid cell centroids or actual earthquake locations?
-lon_centers = lon_edges[1:-2] + grid_spacing  # longitudes of bin centers
-lat_centers = lat_edges[1:-2] + grid_spacing  # latitudes of bin centers
-lon_centers_mat, lat_centers_mat = np.meshgrid(
-    lon_centers, lat_centers
-)  # Plaid gridding of bin centers
-N = len(df)  # number of earthquakes (H2006, eq 1)
-d0 = grid_spacing  # differential degrees for (H2006, eq 3)
+def grid_seismicity(df):
+    grid_spacing = 0.05
+    lon_edges = np.arange(360.0 - 121.55, 360.0 - 114.45 + grid_spacing, grid_spacing)
+    lat_edges = np.arange(32.45, 36.65 + grid_spacing, grid_spacing)
+    H, _, _ = np.histogram2d(
+        df.longitude.values, df.latitude.values, bins=(lon_edges, lat_edges)
+    )
+    H = H.T
+    timespan_years = (df.datetime.max() - df.datetime.min()) / np.timedelta64(1, "Y")
+    H = H / timespan_years
+    H = np.log10(H)
+    H[np.isinf(H)] = np.nan
+    quick_plot(H, "earthquakes per year", lon_edges, lat_edges)
+    return grid_spacing, lon_edges, lat_edges
 
 
-# Let's calculate the back ground rate with an assumed smoothing bandwith
-Kr = calc_kernel_influence(
-    242, 34, lon_centers_mat.flatten(), lat_centers_mat.flatten(), d0
-)
-Kr = Kr.reshape((len(lat_centers), len(lon_centers)))
-# Kr = Kr.T
-plt.figure()
-plt.imshow(
-    Kr,
-    interpolation="nearest",
-    origin="lower",
-    extent=[lon_edges[0], lon_edges[-1], lat_edges[0], lat_edges[-1]],
-)
-plt.show(block=False)
-plt.xlabel("longitude")
-plt.ylabel("latitude")
-plt.colorbar(label="mu_star")
+def main():
+    df = read_data()
+    df = df[df.magnitude > 5.0]  # CLIP TO EVENTS LARGER THAN 5 FOR SPEED
+    grid_spacing, lon_edges, lat_edges = grid_seismicity(df)
+
+    # Helmstetter et al. 2006 calcuations
+    lon_centers = lon_edges[1:-2] + grid_spacing  # longitudes of bin centers
+    lat_centers = lat_edges[1:-2] + grid_spacing  # latitudes of bin centers
+    lon_centers_mat, lat_centers_mat = np.meshgrid(
+        lon_centers, lat_centers
+    )  # Plaid gridding of bin centers
+    N = len(df)  # number of earthquakes (H2006, eq 1)
+    d0 = grid_spacing  # differential degrees for (H2006, eq 3)
+
+    # # Let's calculate the back ground rate with an assumed smoothing bandwith
+    # Kr = calc_kernel_influence(
+    #     242, 34, lon_centers_mat.flatten(), lat_centers_mat.flatten(), d0
+    # )
+    # Kr = Kr.reshape((len(lat_centers), len(lon_centers)))
+    # quick_plot(Kr, "Example kernel", lon_edges, lat_edges)
+
+    # For a single grid cell calcuate the influence of all earthquakes
+    mu_star = np.zeros(lon_centers_mat.flatten().shape)
+    for i in range(0, mu_star.size):
+        mu_star[i] = (
+            sum(
+                calc_kernel_influence(
+                    lon_centers_mat.flatten()[i],
+                    lat_centers_mat.flatten()[i],
+                    df.longitude.values,
+                    df.latitude.values,
+                    d0,
+                )
+            )
+            / N
+        )
+        print(i)
+    mu_star = mu_star.reshape((len(lat_centers), len(lon_centers)))
+    quick_plot(mu_star, "mu_star", lon_edges, lat_edges)
+
+
+if __name__ == "__main__":
+    main()
